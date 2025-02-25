dd39bb5d9c57e81118f77739fb471c9adfae20c3
KirillP <kosmosmakmelov4@gmail.com>
2025-02-25 19:09:14 +0300

    Schedule

---
+++ Schedule.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
+import tkinter as tk
+from tkinter import messagebox
+import time
+import requests
+from PIL import Image, ImageTk
+from fake_useragent import UserAgent
+
+useragent = UserAgent()
+option = webdriver.ChromeOptions()
+option.add_argument(f'user-agent={useragent.chrome}')
+driver = webdriver.Chrome(options = option)
+url = 'https://timetable.vsuet.ru/'
